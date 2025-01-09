b6c2d610020ba095022dbb3ece74e2150464ae90
Felipe Alvarado <felipe@safe.global>
2025-01-09 16:26:00 +0100

    Add proxy implementation address to contract and download metadata

--- app/tests/workers/test_tasks.py
+++ app/tests/workers/test_tasks.py
@@ -1,9 +1,13 @@
 import json
 import unittest
 from typing import Any, Awaitable
+from unittest import mock
+from unittest.mock import MagicMock
 
 from dramatiq.worker import Worker
+from eth_account import Account
 from hexbytes import HexBytes
+from safe_eth.eth.clients import AsyncEtherscanClientV2
 from sqlmodel.ext.asyncio.session import AsyncSession
 
 from app.datasources.db.database import database_session
@@ -11,6 +15,10 @@ from app.datasources.db.models import Contract
 from app.workers.tasks import get_contract_metadata_task, redis_broker, test_task
 
 from ..datasources.db.db_async_conn import DbAsyncConn
+from ..mocks.contract_metadata_mocks import (
+    etherscan_metadata_mock,
+    etherscan_proxy_metadata_mock,
+)
 
 
 class TestTasks(unittest.TestCase):
@@ -62,8 +70,19 @@ class TestAsyncTasks(DbAsyncConn):
         await super().asyncTearDown()
         self.worker.stop()
 
+    def _wait_tasks_execution(self):
+        redis_tasks = self.worker.broker.client.lrange("dramatiq:default", 0, -1)
+        while len(redis_tasks) > 0:
+            redis_tasks = self.worker.broker.client.lrange("dramatiq:default", 0, -1)
+
+    @mock.patch.object(
+        AsyncEtherscanClientV2, "async_get_contract_metadata", autospec=True
+    )
     @database_session
-    async def test_get_contract_metadata_task(self, session: AsyncSession):
+    async def test_get_contract_metadata_task(
+        self, etherscan_get_contract_metadata_mock: MagicMock, session: AsyncSession
+    ):
+        etherscan_get_contract_metadata_mock.return_value = etherscan_metadata_mock
         contract_address = "0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552"
         chain_id = 100
         get_contract_metadata_task.fn(contract_address, chain_id)
@@ -71,3 +90,35 @@ class TestAsyncTasks(DbAsyncConn):
             session, HexBytes(contract_address), chain_id
         )
         self.assertIsNotNone(contract)
+        self.assertEqual(etherscan_get_contract_metadata_mock.call_count, 1)
+
+    @mock.patch.object(
+        AsyncEtherscanClientV2, "async_get_contract_metadata", autospec=True
+    )
+    @database_session
+    async def test_get_contract_metadata_task_with_proxy(
+        self, etherscan_get_contract_metadata_mock: MagicMock, session: AsyncSession
+    ):
+        etherscan_get_contract_metadata_mock.side_effect = [
+            etherscan_proxy_metadata_mock,
+            etherscan_metadata_mock,
+        ]
+        contract_address = Account.create().address
+        proxy_implementation_address = "0x43506849D7C04F9138D1A2050bbF3A0c054402dd"
+        chain_id = 1
+
+        get_contract_metadata_task.fn(address=contract_address, chain_id=chain_id)
+
+        self._wait_tasks_execution()
+
+        contract = await Contract.get_contract(
+            session, HexBytes(contract_address), chain_id
+        )
+        self.assertIsNotNone(contract)
+
+        proxy_implementation = await Contract.get_contract(
+            session, HexBytes(proxy_implementation_address), chain_id
+        )
+        self.assertIsNotNone(proxy_implementation)
+
+        self.assertEqual(etherscan_get_contract_metadata_mock.call_count, 2)
