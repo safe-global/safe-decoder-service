a546747b8be6fee1854007aeba33e46d5ffdfc9e
Uxio Fuentefria <6909403+Uxio0@users.noreply.github.com>
2025-01-24 11:34:57 +0100

    Index contracts inside MultiSend calls
    - Closes #67

--- app/services/events.py
+++ app/services/events.py
@@ -1,12 +1,30 @@
 import json
 import logging
 
+from eth_typing import ChecksumAddress, HexStr
+from hexbytes import HexBytes
 from safe_eth.eth.utils import fast_is_checksum_address
+from safe_eth.safe.multi_send import MultiSend
 
 from ..workers.tasks import get_contract_metadata_task
 
 
 class EventsService:
+
+    def get_contracts_from_data(self, data: HexStr | None) -> set[ChecksumAddress]:
+        """
+        Extract contract addresses involved in the transaction from `data`.
+        Currently only MultiSend calls are decoded
+
+        :param data:
+        :return: Contract addresses involved in the transaction
+        """
+        if not data:
+            return set()
+        return {
+            multisend_tx.to
+            for multisend_tx in MultiSend.from_transaction_data(HexBytes(data))
+        }
 
     def process_event(self, message: str) -> None:
         """
@@ -17,11 +35,17 @@ class EventsService:
         try:
             tx_service_event = json.loads(message)
             if self._is_processable_event(tx_service_event):
-                chain_id = int(tx_service_event["chainId"])
-                contract_address = tx_service_event["to"]
-                get_contract_metadata_task.send(
-                    address=contract_address, chain_id=chain_id
+                chain_id: int = int(tx_service_event["chainId"])
+                to: ChecksumAddress = tx_service_event["to"]
+                data: HexStr | None = tx_service_event["data"]
+                contracts_from_data: set[ChecksumAddress] = (
+                    self.get_contracts_from_data(data)
                 )
+
+                for contract_address in {to, *contracts_from_data}:
+                    get_contract_metadata_task.send(
+                        address=contract_address, chain_id=chain_id
+                    )
         except json.JSONDecodeError:
             logging.error(f"Unsupported message. Cannot parse as JSON: {message}")
 
@@ -34,15 +58,17 @@ class EventsService:
         :param tx_service_event: The event object to validate.
         :return: `True` if the event is valid, `False` otherwise.
         """
-        chain_id = tx_service_event.get("chainId")
         event_type = tx_service_event.get("type")
+        chain_id = tx_service_event.get("chainId")
         address = tx_service_event.get("to")
+        data = tx_service_event.get("data", 0)  # Data must be present
 
         return (
-            isinstance(chain_id, str)
-            and chain_id.isdigit()
-            and isinstance(event_type, str)
+            isinstance(event_type, str)
             and event_type == "EXECUTED_MULTISIG_TRANSACTION"
+            and isinstance(chain_id, str)
+            and chain_id.isdigit()
+            and (data is None or isinstance(data, str))
             and isinstance(address, str)
             and fast_is_checksum_address(address)
         )
--- app/tests/services/test_events.py
+++ app/tests/services/test_events.py
@@ -1,44 +1,73 @@
 import json
 import unittest
-from unittest.mock import MagicMock, patch
+from unittest.mock import MagicMock, call, patch
+
+from eth_typing import HexStr
 
 from app.services.events import EventsService
+from app.tests.services.mocks_multisend import multisend_data
 
 
 class TestEventsService(unittest.TestCase):
 
-    def test_is_event_valid(self):
+    def test_get_contracts_from_data(self):
+        events_service = EventsService()
+        self.assertEqual(events_service.get_contracts_from_data(None), set())
+        self.assertEqual(events_service.get_contracts_from_data(HexStr("0x")), set())
+        self.assertEqual(events_service.get_contracts_from_data(HexStr("0x8")), set())
+        self.assertEqual(
+            events_service.get_contracts_from_data(HexStr(multisend_data.hex())),
+            {"0x5B9ea52Aaa931D4EEf74C8aEaf0Fe759434FeD74"},
+        )
+
+    def test_is_processable_event(self):
+        not_valid_event = {
+            "chainId": "123",
+            "type": "EXECUTED_MULTISIG_TRANSACTION",
+            "to": "0x6ED857dc1da2c41470A95589bB482152000773e9",
+        }
+        self.assertFalse(EventsService._is_processable_event(not_valid_event))
+
         valid_event = {
             "chainId": "123",
             "type": "EXECUTED_MULTISIG_TRANSACTION",
             "to": "0x6ED857dc1da2c41470A95589bB482152000773e9",
+            "data": None,
         }
         self.assertTrue(EventsService._is_processable_event(valid_event))
 
         valid_event = {
             "chainId": "123",
+            "type": "EXECUTED_MULTISIG_TRANSACTION",
+            "to": "0x6ED857dc1da2c41470A95589bB482152000773e9",
+            "data": "0x1234",
+        }
+        self.assertTrue(EventsService._is_processable_event(valid_event))
+
+        not_valid_event = {
+            "chainId": "123",
             "type": "transaction",
             "to": "0x6ed857dc1da2c41470A95589bB482152000773e9",
         }
-        self.assertFalse(EventsService._is_processable_event(valid_event))
+        self.assertFalse(EventsService._is_processable_event(not_valid_event))
 
-        valid_event = {
+        not_valid_event = {
             "chainId": "123",
             "type": "EXECUTED_MULTISIG_TRANSACTION",
             "to": "0x123456789",
         }
-        self.assertFalse(EventsService._is_processable_event(valid_event))
+        self.assertFalse(EventsService._is_processable_event(not_valid_event))
 
-        valid_event = {
+        not_valid_event = {
             "chainId": "chainId",
             "type": "EXECUTED_MULTISIG_TRANSACTION",
             "to": "0x6ED857dc1da2c41470A95589bB482152000773e9",
         }
-        self.assertFalse(EventsService._is_processable_event(valid_event))
+        self.assertFalse(EventsService._is_processable_event(not_valid_event))
 
-        invalid_event_missing_chain_id = {"type": "transaction"}
+        not_valid_event_missing_chain_id = {"type": "transaction"}
         self.assertFalse(
-            EventsService._is_processable_event(invalid_event_missing_chain_id)
+            EventsService._is_processable_event(not_valid_event_missing_chain_id)
         )
 
         invalid_event_missing_type = {"chainId": "123"}
@@ -67,19 +96,43 @@ class TestEventsService(unittest.TestCase):
         )
 
     @patch("app.workers.tasks.get_contract_metadata_task.send")
-    def test_process_event_calls_send(self, mock_get_contract_metadata_task):
-        mock_get_contract_metadata_task.send = MagicMock()
-
+    def test_process_event_calls_send(self, mock_get_contract_metadata_task: MagicMock):
         valid_message = json.dumps(
             {
                 "chainId": "1",
                 "type": "EXECUTED_MULTISIG_TRANSACTION",
                 "to": "0x6ED857dc1da2c41470A95589bB482152000773e9",
+                "data": None,
             }
         )
 
+        mock_get_contract_metadata_task.assert_not_called()
         EventsService().process_event(valid_message)
-
         mock_get_contract_metadata_task.assert_called_once_with(
             address="0x6ED857dc1da2c41470A95589bB482152000773e9", chain_id=1
+        )
+
+    @patch("app.workers.tasks.get_contract_metadata_task.send")
+    def test_process_event_with_multisend_calls_send(
+        self, mock_get_contract_metadata_task: MagicMock
+    ):
+        valid_message = json.dumps(
+            {
+                "chainId": "1",
+                "type": "EXECUTED_MULTISIG_TRANSACTION",
+                "to": "0x6ED857dc1da2c41470A95589bB482152000773e9",
+                "data": HexStr(multisend_data.hex()),
+            }
+        )
+
+        mock_get_contract_metadata_task.assert_not_called()
+        events_service = EventsService()
+        events_service.process_event(valid_message)
+        self.assertEqual(mock_get_contract_metadata_task.call_count, 2)
+        mock_get_contract_metadata_task.assert_has_calls(
+            [
+                call(address="0x6ED857dc1da2c41470A95589bB482152000773e9", chain_id=1),
+                call(address="0x5B9ea52Aaa931D4EEf74C8aEaf0Fe759434FeD74", chain_id=1),
+            ],
+            any_order=True,
         )
