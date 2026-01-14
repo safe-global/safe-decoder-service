ece4d12c3d4b42b24e382f6efb220f0d276c1273
Felipe Alvarado <6717781+falvaradorodriguez@users.noreply.github.com>
2026-01-14 14:08:47 +0100

    Add models to env migrations for autogenerate support

--- migrations/env.py
+++ migrations/env.py
@@ -8,6 +8,7 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 from sqlmodel import SQLModel
 
 from app.config import settings
+from app.datasources.db.models import *  # noqa: F403 - Required for autogenerate support
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
